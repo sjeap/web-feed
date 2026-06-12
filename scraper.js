07cf4e783a90beb0d321f7c6d68a97044518d4fd
sjeap <sjeap@krutt.org>
2026-06-12 03:17:26 +0200

    Add files via upload

--- scraper.js
+++ scraper.js
@@ -68,11 +68,21 @@ function fetchPageHttps(url, redirectCount = 0) {
       headers: {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
-          "(KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
-        "Accept":          "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Accept-Language": "de-DE,de;q=0.9,en;q=0.8",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Cache-Control":   "no-cache",
+          "(KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36",
+        "Accept":
+          "text/html,application/xhtml+xml,application/xml;q=0.9," +
+          "image/avif,image/webp,image/apng,*/*;q=0.8," +
+          "application/signed-exchange;v=b3;q=0.7",
+        "Accept-Language":           "de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Accept-Encoding":           "gzip, deflate, br",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest":            "document",
+        "Sec-Fetch-Mode":            "navigate",
+        "Sec-Fetch-Site":            "none",
+        "Sec-Fetch-User":            "?1",
+        "sec-ch-ua":                 '"Chromium";v="149", "Google Chrome";v="149", "Not?A_Brand";v="99"',
+        "sec-ch-ua-mobile":          "?0",
+        "sec-ch-ua-platform":        '"Windows"',
       },
     };
 
